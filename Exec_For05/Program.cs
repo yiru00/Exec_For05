6f61500692c55fb68f3fd69797344208a182e411
yiru00 <eva890706@gmail.com>
2022-10-16 23:09:33 +0800

    作業

---
+++ Exec_For05/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            string plus = new string('+', 5 - i);
+            System.Console.Write(plus);
+
+            for (int j = 1; j <= i; j++)
+            {
+                System.Console.Write(i);
+            }
+
+            System.Console.WriteLine("");
+        }
+    }
+}
